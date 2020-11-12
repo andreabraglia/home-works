51a67de5b3eb66c98890a3cbe0c269b7b0b47f38
andreabraglia <andrea.braglia.ls@gmail.com>
2020-11-12 18:03:08 +0100

    add image api

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const handlebars = require("express-handlebars")
+const fetch = require("node-fetch")
+
 const app = new express()
 const port = process.argv.slice(2)[0] === "--port" ? process.argv.slice(2)[1] : 8080
 
@@ -18,6 +20,7 @@ const badRequest = (message = "Bad request", status = 400) => {
 }
 
 app.set("view engine", "handlebars")
+app.enable("view cache")
 app.engine(
   "handlebars",
   handlebars({
@@ -31,22 +34,41 @@ app.engine(
 app.use(require("body-parser").json())
 app.use(express.static(folder.public))
 
-
-app.get("/", (req, res) => {
-  res.render("main", { about: true, projects: true, contact: true })
-})
-
 ;[
+  { path: "", filler: { about: true, projects: true, contact: true } },
   { path: "About", filler: { about: true } },
-  { path: "Projects", filler: { projects: true } },
-  { path: "Contact", filler: { contact: true } }
-].forEach(({ path, filler }) => {
-  app.get(`/${path}`, (_, res) => res.render("main", filler))
+  {
+    path: "Projects", filler: { projects: true }, callback: async (req, res) => {
+      const page = req.query.page || 1
+      console.log(page)
+      try {
+        let imgs = await fetch(`https://picsum.photos/v2/list?page=${page}&limit=4`)
+        imgs = await imgs.json()
+        imgs = imgs.reduce((acc, obj, index) => {
+          acc.push({ ...obj, index: !!(index % 2) })
+          return acc
+        }, [])
+        res.render("main", { projects: true, imgs })
+      } catch (err) {
+        console.log(err)
+        res.send(badRequest("Api image seems down", 500))
+      }
+    }
+  },
+  { path: "Contact", filler: { contact: true } },
+  {
+    path: "Subscribe", method: "post", callback: ({ body }, res) => {
+      console.log(body)
+      res.send({ status: 200 })
+    }
+  }
+].forEach(({ path, method, filler, callback }) => {
+  app[method || "get"](`/${path}`, callback ? callback : (_, res) => res.render("main", filler))
 })
 
 
 app.all("*", (_, res) => {
-  res.send("<div style='margin: auto; text-align: center'><h1>404</h1> <br/> <h3>RESOURCE NOT FOUND</h3></div>")
+  res.render("error")
 })
 
 app.listen(port, () => {
