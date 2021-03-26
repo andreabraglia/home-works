4c1f7071d3f65bbec6f70158f135b839fe56fcee
andreabraglia <andrea.braglia.ls@gmail.com>
2021-03-26 20:24:10 +0100

    add bootstrap

--- battaglia-navale/index.js
+++ battaglia-navale/index.js
@@ -77,14 +77,20 @@ app.get("/", ({ query: { format } }, res) => {
       `<!DOCTYPE html>
               <html lang="it">
                 <head>
+                 <meta charset="utf-8">
+                 <meta name="viewport" content="width=device-width, initial-scale=1">
+
+                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
                   <title>battaglia navale</title>
                   <style>
                     body {
-                      width: 100%;
-                      height: 100%;
+                      width: 100vw;
+                      height: 100vh;
                       margin: 0;
                       padding: 0;
                       align-content: center;
+                      align-items: center;
                     }
                     
                     table, td, th {
@@ -115,11 +121,12 @@ app.get("/", ({ query: { format } }, res) => {
                   </style>
                 </head>
                 <body>
-                  <table>
+                  <table class="align-middle">
                     <tbody>
                       ${formattedField}
                     </tbody>
                   </table>
+                  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
                 </body>
               </html>`
     )
